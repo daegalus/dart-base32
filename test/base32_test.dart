70415cf66ac7ffc07409570b23a85efb9ea26a08
Yulian Kuncheff <yulian@kuncheff.com>
2012-11-01 15:56:01 -0700

    Initial Commit, working, tests pass.

---
+++ test/base32_test.dart
@@ -0,0 +1,24 @@
+import 'package:unittest/unittest.dart';
+import 'package:base32/base32.dart';
+import 'dart:crypto';
+import 'dart:scalarlist';
+
+void main() {
+  base32 base32 = new base32();
+
+  group('[Decode]', () {
+    test('JBSWY3DPEHPK3PXP -> 48656c6c6f21deadbeef', () {
+      var decoded = base32.decode("JBSWY3DPEHPK3PXP");
+      var decodedString = CryptoUtils.bytesToHex(decoded);
+
+      expect(decodedString.toString(), equals('48656c6c6f21deadbeef'));
+    });
+  });
+
+  group('[Encode]', () {
+    test('48656c6c6f21deadbeef -> JBSWY3DPEHPK3PXP', () {
+      var decoded = base32.encodeHexString("48656c6c6f21deadbeef");
+      expect(decoded, equals('JBSWY3DPEHPK3PXP'));
+    });
+  });
+}
